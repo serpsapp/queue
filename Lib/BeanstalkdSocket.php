0444b63523b7d6f47a228a553d5fa3725836304c
Adam Bliss <abliss@serps.com>
2015-04-27 14:31:52 -0700

    Cherry-pick upstream change that allows job sizes > 16k.
    See: https://github.com/davidpersson/beanstalk/commit/528a82

--- Lib/BeanstalkdSocket.php
+++ Lib/BeanstalkdSocket.php
@@ -203,7 +203,7 @@ class BeanstalkdSocket {
 			if (feof($this->_connection)) {
 				return false;
 			}
-			$data = fread($this->_connection, $length + 2);
+			$data = stream_get_contents($this->_connection, $length + 2);
 			$meta = stream_get_meta_data($this->_connection);
 
 			if ($meta['timed_out']) {
